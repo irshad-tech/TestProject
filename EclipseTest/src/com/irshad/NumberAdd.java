515a2b09e0d4a8391b659ae71e415bd35771b021
irshad-tech <irshad3030@gmail.com>
2020-08-01 19:01:57 -0400

    Irshad: initial commit

---
+++ EclipseTest/src/com/irshad/NumberAdd.java
@@ -0,0 +1,12 @@
+package com.irshad;
+
+public class NumberAdd {
+
+	public static void main(String[] args) {
+
+		int num1 = 10, num2 = 10, sum;
+		sum = num1 + num2;
+		System.out.println("Sum of two numbers : " + sum);
+	}
+
+}
